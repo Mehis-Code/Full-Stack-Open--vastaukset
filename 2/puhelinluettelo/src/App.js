72496ab7b5d1f362ecb69ac4806ee6b0c8d215d9
Mehis <vikkek@gmail.com>
2023-05-10 15:57:02 +0300

    2.15

--- 2/puhelinluettelo/src/App.js
+++ 2/puhelinluettelo/src/App.js
@@ -1,16 +1,31 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import services from './services/nettijutut';
 
 const App = () => {
-  const [persons, setPersons] = useState([    
-  { id: 1, name: 'Arto Hellas', number: '040-123456' },
-  { id: 2,  name: 'Ada Lovelace', number: '39-44-5323523' },
-  { id: 3,  name: 'Dan Abramov', number: '12-43-234345' },
-  {  id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumb, setNewNumb] = useState('')
   const [filterA, setFilter] = useState('')
-  const [newArr, setNewArr] = useState(persons)
+  const [newArr, setNewArr] = useState([])
+
+  useEffect(() => {
+    services.getAll().then(response => {
+      setPersons(response);
+      setNewArr(response)
+    });
+  }, []);
+
+  const handleDelete = (id, name) => {
+    if (window.confirm('delete ' + name + ' ?') != true) {
+      return;
+    }
+    services.remove(id).then(() => {
+      const updatedPersons = persons.filter(person => person.id !== id);
+      setPersons(updatedPersons);
+      setNewArr(updatedPersons);
+      
+    });
+  };
 
   return (
     <div>
@@ -23,7 +38,7 @@ const App = () => {
         setNewNumb={setNewNumb}
         setPersons={setPersons} 
         setNewArr={setNewArr}/>
-      <Persons persons={newArr} />
+      <Persons persons={newArr} handleDelete={handleDelete}/>
     </div>
   )
 }
@@ -44,11 +59,12 @@ const Phonebook = ({persons, filter, setFilter, setNewArr}) => {
   )
 }
 
-const Persons = ( {persons}) => {
+const Persons = ( {persons, handleDelete}) => {
   return(
   <div>
     <h2>Numbers</h2>
-    {persons.map((person) => (<p key={person.id}>{person.name} {person.number}</p>))}
+    {persons.map((person) => (<p key={person.id}>{person.name} {person.number} <button key = {person.id} onClick={(  
+    ) => handleDelete(person.id, person.name)}> delete</button></p>))}
   </div>
   )
 }
@@ -63,11 +79,30 @@ const PersonForm = ({ persons, newName, newNumb, setNewName, setNewNumb, setPers
   const newP = (event) => {
     const a = persons.map((x) => (x.name))
     if (a.includes(newName)){
-      event.preventDefault()
-      setNewName('')
-      setNewNumb('')
-      alert(`${newName} is already added to phonebook`)
-      return;
+      const o = window.confirm(`${newName} is already added to the phonebook, replace the old number with a new one?`);
+      if (o == false){
+        event.preventDefault()
+        setNewName('')
+        setNewNumb('')
+        return;
+      }
+      else {
+        event.preventDefault()
+        const m = persons.filter((x) => x.name == newName);
+        const newC = {
+          id: m[0].id,
+          name: newName,
+          number: newNumb
+        }
+        let b = persons.filter(x => x.id != m[0].id);
+        services.update(m[0].id, newC).then(() => {
+          setPersons(b.concat(newC))
+          setNewArr(b.concat(newC))
+          setNewName('')
+          setNewNumb('')
+        });
+        return;
+      }
     }
 
     event.preventDefault()
@@ -76,10 +111,14 @@ const PersonForm = ({ persons, newName, newNumb, setNewName, setNewNumb, setPers
       name: newName,
       number: newNumb
     }
-    setPersons(persons.concat(pObject))
-    setNewArr(persons.concat(pObject))
-    setNewName('')
-    setNewNumb('')
+
+    services.create(pObject).then(thing => {
+      setPersons(persons.concat(pObject))
+      setNewArr(persons.concat(pObject))
+      setNewName('')
+      setNewNumb('')
+    })
+
       }  
       return (
     <div>
